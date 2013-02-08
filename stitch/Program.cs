61e3fae33397912f76d0eda4fb574805839c036b
Jeffrey Crowell <crowell@bu.edu>
2013-02-08 16:18:57 -0500

    dump code, needs cropping before usable

---
+++ stitch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace stitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> pictures = new List<string>();
+            for (int ii = 1; ii < 11; ii++)
+            {
+                pictures.Add(@"C:\Users\jeff\Dropbox\Algorthmics-TestDrive\PDFRequirement\Full2"+@"\image"+ii+".png");
+            }
+            Bitmap bitmap = Stitcher.Combine(pictures.ToArray());
+            bitmap.Save("bitmap.jpg");
+        }
+    }
+}
---
+++ stitch/Shred.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stitch
+{
+    class Shred
+    {
+        public string Path;
+        public int Offset;
+        public Shred(string path, int offset)
+        {
+            this.Path = path;
+            this.Offset = offset;
+        }
+    }
+}
